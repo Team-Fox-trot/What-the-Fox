9d2aaaff1f69129004330030d63e1817cc198283
Trey Young(goodpudding) <james.trey.young@gmail.com>
2023-03-22 04:39:09 -0700

    clicking submit now brings you to the favorites page

--- src/FoxCarousel.js
+++ src/FoxCarousel.js
@@ -4,6 +4,8 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Container } from 'react-bootstrap';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from "react-router-dom";
+
 
 function FoxCarousel(props) {
   //auth0
@@ -14,6 +16,7 @@ function FoxCarousel(props) {
     console.log(selectedIndex);
     setIndex(selectedIndex);
   };
+  const navigate = useNavigate();
 
   const foxHandler = (e) =>{
     e.preventDefault();
@@ -21,8 +24,8 @@ function FoxCarousel(props) {
       userInput: props.userInput,
       imgURL: props.allFoxes[index].src,
     }
-    console.log(newFoxMeme);
     props.handleFoxSubmit(newFoxMeme);
+    navigate("/favorites");
   }
     return (
       <Container>
