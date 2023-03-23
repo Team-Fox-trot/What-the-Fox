771be99251e5c2c9552d15ea8bee51a965796abe
Thomas Lau <tomslau@outlook.com>
2023-03-22 21:38:57 -0700

    fix

--- src/Footer.js
+++ src/Footer.js
@@ -7,8 +7,7 @@ class Footer extends React.Component {
     return (
       <>
         <div>
-          <p className='p'>footer</p>
-          <img className='Grass' src='/Assests/GrassBottom.png' alt='' />
+         <img className='Grass' src='/Assests/GrassBottom.png' alt='' />
           <img className='Ears' src='/Assests/Fox-ears-transparent.png' alt='' />
         </div>
       </>
